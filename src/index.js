cba8e0c203541f0a69fde5ef2d9b07eb1f58703f
Milton Castro <milton.castro@inovant.com.br>
2019-11-25 01:03:31 -0300

    chore (deps): add support to TypeScript

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase/app';
 import firebaseConfig from 'firebaseConfig.js';
 import { FirestoreProvider } from 'react-firestore';
@@ -15,4 +15,4 @@ ReactDOM.render(
     <App />
   </FirestoreProvider>
   , document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.unregister();
