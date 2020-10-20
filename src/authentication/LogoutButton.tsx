1919a444e00cd748762289775f719c7f19ddb2b9
Milton Castro <milton.castro@inovant.com.br>
2020-10-20 03:13:04 -0300

    refactor(typescript): convert LogoutButton to TypeScript

--- src/authentication/LogoutButton.tsx
+++ src/authentication/LogoutButton.tsx
@@ -1,40 +1,36 @@
-import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import React, { FunctionComponent } from 'react';
 import { Button, Image, Item, Popup } from 'semantic-ui-react';
 
 const handleLogout = () => {
   firebase.auth().signOut();
 };
 
-export default () => {
-  const user = firebase.auth().currentUser;
-  const initials = (user.displayName
-    ? user.displayName
-        .split(' ')
-        .map((name) => name.substring(0, 1))
-        .join('')
-    : user.email.substring(0, 1)
-  ).toUpperCase();
-  const avatar = user.photoURL ? (
-    <Image circular bordered size="mini" src={user.photoURL} />
+const LogoutButton: FunctionComponent = () => {
+  const { displayName, email, photoURL } = firebase.auth().currentUser ?? {};
+  const avatar = photoURL ? (
+    <Image circular bordered size="mini" src={photoURL} />
   ) : (
     <svg viewBox="0 0 24 24" height="32px">
       <circle fill="#a0d36a" cx="12" cy="12" r="12"></circle>
       <text fill="#ffffff" font-size="12" text-anchor="middle" x="12" y="16">
-        {initials}
+        {displayName
+          ?.split(' ')
+          .map((name) => name.substring(0, 1))
+          .join('') || email?.substring(0, 1).toUpperCase()}
       </text>
     </svg>
   );
 
   return (
-    <Popup on="click" size="large" position="top right" trigger={avatar} offset={5}>
+    <Popup on="click" size="large" position="top right" trigger={avatar} offset={[5, 0]}>
       <Item.Group>
         <Item style={{ width: '350px' }}>
-          <Item.Image size="tiny" src={user.photoURL} />
+          <Item.Image size="tiny" src={photoURL} />
           <Item.Content>
-            <Item.Header>{user.displayName}</Item.Header>
-            <Item.Meta>{user.email}</Item.Meta>
+            <Item.Header>{displayName}</Item.Header>
+            <Item.Meta>{email}</Item.Meta>
             <Item.Extra>
               <Button onClick={handleLogout} floated="right">
                 Sair
@@ -46,3 +42,5 @@ export default () => {
     </Popup>
   );
 };
+
+export default LogoutButton;
