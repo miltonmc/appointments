45cc5c7fc7fa4041f2d00eebb45c943556b8dc46
Milton Castro <milton.castro@inovant.com.br>
2021-03-08 01:01:50 -0300

    refactor: convert Item to function component

--- src/calendar/Item.js
+++ src/calendar/Item.js
@@ -1,25 +1,38 @@
+import moment from 'moment';
+import React, { useState } from 'react';
 import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 import CustomerField from './CustomerField';
-import moment from 'moment';
-import React, { Component } from 'react';
 import HealthPlanField from './HealthPlanField';
 
-export default class Item extends Component {
-  state = {
-    startDate: moment(this.props.start || Date.now()).format('YYYY-MM-DD'),
-    startTime: moment(this.props.start || Date.now()).format('HH:mm'),
-    endDate: moment(this.props.end || Date.now()).format('YYYY-MM-DD'),
-    endTime: moment(this.props.end || Date.now()).format('HH:mm'),
-    customer: this.props.customer || '',
-    healthPlanId: this.props.healthPlanId || '',
-    notes: this.props.notes || '',
-  };
+export default function CalendarItem({
+  id,
+  errorMessage,
+  healthPlanId: healthPlanIdProp,
+  healthPlans,
+  onClose,
+  onSubmit,
+  title,
+  start,
+  end,
+  customer: customerProp,
+  notes: notesProp,
+}) {
+  const [state, setState] = useState({
+    startDate: moment(start ?? Date.now()).format('YYYY-MM-DD'),
+    startTime: moment(start ?? Date.now()).format('HH:mm'),
+    endDate: moment(end ?? Date.now()).format('YYYY-MM-DD'),
+    endTime: moment(end ?? Date.now()).format('HH:mm'),
+    customer: customerProp ?? '',
+    healthPlanId: healthPlanIdProp ?? '',
+    notes: notesProp ?? '',
+  });
+  const { startDate, startTime, endDate, endTime, customer, healthPlanId, notes } = state;
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+  function handleChange(_e, { name, value }) {
+    setState({ ...state, [name]: value });
+  }
 
-  handleSubmit = () => {
-    const { startDate, startTime, endDate, endTime, customer, notes, healthPlanId } = this.state;
-    const { id, onSubmit } = this.props;
+  function handleSubmit() {
     const start = moment(`${startDate} ${startTime}`).toDate();
     const end = moment(`${endDate} ${endTime}`).toDate();
     const customerData = {
@@ -27,48 +40,37 @@ export default class Item extends Component {
       name: customer.name,
     };
     onSubmit({ id, start, end, customer: customerData, healthPlanId, notes });
-  };
-
-  render() {
-    const { startDate, startTime, endDate, endTime, customer, notes, healthPlanId } = this.state;
-    const { errorMessage, healthPlans, onClose, title } = this.props;
-    return (
-      <Modal size="tiny" open onClose={onClose}>
-        <Modal.Header content={title} />
-        <Modal.Content>
-          <Form onSubmit={this.handleSubmit} error={!!errorMessage} size="mini">
-            <Form.Group>
-              <Form.Input
-                width={7}
-                type="date"
-                name="startDate"
-                value={startDate}
-                onChange={this.handleChange}
-                required
-              />
-              <Form.Input type="time" name="startTime" value={startTime} onChange={this.handleChange} required />
-              <div className="calendar__new-item">até</div>
-              <Form.Input width={7} type="date" name="endDate" value={endDate} required onChange={this.handleChange} />
-              <Form.Input type="time" name="endTime" value={endTime} onChange={this.handleChange} required />
-            </Form.Group>
-            <CustomerField selectedCustomer={customer} onChange={this.handleChange} />
-            <HealthPlanField
-              healthPlans={healthPlans}
-              selectedCustomer={customer}
-              selectedHealthPlanId={healthPlanId}
-              onChange={this.handleChange}
-            />
-            <Form.TextArea label="Notas" rows="3" name="notes" value={notes} onChange={this.handleChange} />
-            <Message error header="Dados inválidos" content={errorMessage} />
-            <Button color="blue">
-              <Icon name="checkmark" /> Salvar
-            </Button>
-            <Button type="button" onClick={onClose}>
-              <Icon name="close" /> Cancelar
-            </Button>
-          </Form>
-        </Modal.Content>
-      </Modal>
-    );
   }
+
+  return (
+    <Modal size="tiny" open onClose={onClose}>
+      <Modal.Header content={title} />
+      <Modal.Content>
+        <Form onSubmit={handleSubmit} error={!!errorMessage} size="mini">
+          <Form.Group>
+            <Form.Input width={7} type="date" name="startDate" value={startDate} onChange={handleChange} required />
+            <Form.Input type="time" name="startTime" value={startTime} onChange={handleChange} required />
+            <div className="calendar__new-item">até</div>
+            <Form.Input width={7} type="date" name="endDate" value={endDate} required onChange={handleChange} />
+            <Form.Input type="time" name="endTime" value={endTime} onChange={handleChange} required />
+          </Form.Group>
+          <CustomerField selectedCustomer={customer} onChange={handleChange} />
+          <HealthPlanField
+            healthPlans={healthPlans}
+            selectedCustomer={customer}
+            selectedHealthPlanId={healthPlanId}
+            onChange={handleChange}
+          />
+          <Form.TextArea label="Notas" rows="3" name="notes" value={notes} onChange={handleChange} />
+          <Message error header="Dados inválidos" content={errorMessage} />
+          <Button color="blue">
+            <Icon name="checkmark" /> Salvar
+          </Button>
+          <Button type="button" onClick={onClose}>
+            <Icon name="close" /> Cancelar
+          </Button>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  );
 }
