bc1a0acea0c4b4d2cbfad4a2c37c9c7f90165b10
Milton Castro <milton.castro@inovant.com.br>
2021-03-28 00:51:35 -0300

    refactor: convert HealthPlanEdit to function component

--- src/health-plan/EditItem.js
+++ src/health-plan/EditItem.js
@@ -1,42 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { withFirestore } from 'react-firestore';
 import FirestorePath from '../components/FirestorePath';
 import Item from './Item.js';
 
-class EditItem extends Component {
-  state = {};
+const HealthPlanEdit = ({ item, firestore, onClose }) => {
+  const [errorMessage, setErrorMessage] = useState();
 
-  handleSubmit = (fullPath, id, cnpj, name) => {
-    const { firestore, onClose } = this.props;
-    firestore
-      .doc(`${fullPath}/${id}`)
-      .update({ name })
-      .then(() => onClose('Convênio atualizado com sucesso.'))
-      .catch((error) => {
-        this.setState({
-          errorMessage: error,
-        });
-      });
+  const handleSubmit = async (fullPath, id, _cnpj, name) => {
+    try {
+      await firestore.doc(`${fullPath}/${id}`).update({ name });
+      onClose('Convênio atualizado com sucesso.');
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
   };
 
-  render() {
-    const { errorMessage } = this.state;
-    const { item, onClose } = this.props;
-    return (
-      <FirestorePath
-        path="HealthPlans"
-        render={(fullPath) => (
-          <Item
-            title="Convênio"
-            {...item}
-            errorMessage={errorMessage}
-            onSubmit={(...props) => this.handleSubmit(fullPath, ...props)}
-            onClose={onClose}
-          />
-        )}
-      />
-    );
-  }
-}
+  return (
+    <FirestorePath
+      path="HealthPlans"
+      render={(fullPath) => (
+        <Item
+          title="Convênio"
+          {...item}
+          errorMessage={errorMessage}
+          onSubmit={(...props) => handleSubmit(fullPath, ...props)}
+          onClose={onClose}
+        />
+      )}
+    />
+  );
+};
 
-export default withFirestore(EditItem);
+export default withFirestore(HealthPlanEdit);
