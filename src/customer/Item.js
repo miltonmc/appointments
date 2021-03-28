33509f8b2debe49fa527e488a2510e30cf5c85c8
Milton Castro <milton.castro@inovant.com.br>
2021-03-28 18:46:12 -0300

    refactor: convert Customer to function component

--- src/customer/Item.js
+++ src/customer/Item.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 import CPF from 'cpf';
-import HealthPlanSelect from '../shared/HealthPlanSelect';
+import React, { useState } from 'react';
+import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 import StateSelect from '../components/StateSelect';
+import HealthPlanSelect from '../shared/HealthPlanSelect';
 import Sponsor from './Sponsor';
 
 const addressTypes = [
@@ -16,232 +16,144 @@ const genders = [
   { key: 'm', value: 'm', text: 'Masculino' },
 ];
 
-export default class Item extends Component {
-  state = {
-    addressType: this.props.addressType || '',
-    address: this.props.address || '',
-    address2: this.props.address2 || '',
-    neighborhood: this.props.neighborhood || '',
-    birth: this.props.birth || '',
-    rg: this.props.rg || '',
-    cpf: this.props.cpf || '',
-    email: this.props.email || '',
-    gender: this.props.gender || '',
-    healthPlanId: this.props.healthPlanId || '',
-    name: this.props.name || '',
-    number: this.props.number || '',
-    city: this.props.city || '',
-    state: this.props.state || '',
-    zip: this.props.zip || '',
-    cell: this.props.cell || '',
-    home: this.props.home || '',
-    work: this.props.work || '',
-    facebook: this.props.facebook || '',
-    instagram: this.props.instagram || '',
-    twitter: this.props.twitter || '',
-    linkedin: this.props.linkedin || '',
-    sponsor: this.props.sponsor || '',
-  };
+const Customer = ({ errorMessage, healthPlans, onClose, title, id, onSubmit, ...customerProp }) => {
+  const [customer, setCustomer] = useState({
+    addressType: customerProp.addressType ?? '',
+    address: customerProp.address ?? '',
+    address2: customerProp.address2 ?? '',
+    neighborhood: customerProp.neighborhood ?? '',
+    birth: customerProp.birth ?? '',
+    rg: customerProp.rg ?? '',
+    cpf: customerProp.cpf ?? '',
+    email: customerProp.email ?? '',
+    gender: customerProp.gender ?? '',
+    healthPlanId: customerProp.healthPlanId ?? '',
+    name: customerProp.name ?? '',
+    number: customerProp.number ?? '',
+    city: customerProp.city ?? '',
+    state: customerProp.state ?? '',
+    zip: customerProp.zip ?? '',
+    cell: customerProp.cell ?? '',
+    home: customerProp.home ?? '',
+    work: customerProp.work ?? '',
+    facebook: customerProp.facebook ?? '',
+    instagram: customerProp.instagram ?? '',
+    twitter: customerProp.twitter ?? '',
+    linkedin: customerProp.linkedin ?? '',
+    sponsor: customerProp.sponsor ?? '',
+  });
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+  const {
+    addressType,
+    address,
+    address2,
+    neighborhood,
+    birth,
+    city,
+    rg,
+    cpf,
+    email,
+    gender,
+    healthPlanId,
+    name,
+    number,
+    state,
+    zip,
+    cell,
+    home,
+    work,
+    facebook,
+    instagram,
+    twitter,
+    linkedin,
+    sponsor,
+  } = customer;
 
-  handleSubmit = () => {
-    const {
-      addressType,
-      address,
-      address2,
-      neighborhood,
-      birth,
-      city,
-      rg,
-      cpf,
-      email,
-      gender,
-      healthPlanId,
-      name,
-      number,
-      state,
-      zip,
-      cell,
-      home,
-      work,
-      facebook,
-      instagram,
-      twitter,
-      linkedin,
-      sponsor,
-    } = this.state;
-    const { id, onSubmit } = this.props;
+  const handleChange = (e, { name, value }) => setCustomer((customer) => ({ ...customer, [name]: value }));
 
-    let errorMessage;
-    const customer = { name };
+  const handleSubmit = () => {
+    const errorMessage = !cpf && CPF.isValid(cpf) ? null : 'CPF inválido';
 
-    if (!cpf || CPF.isValid(cpf)) {
-      customer.cpf = cpf;
-      customer.addressType = addressType;
-      customer.address = address;
-      customer.address2 = address2;
-      customer.neighborhood = neighborhood;
-      customer.birth = birth;
-      customer.rg = rg;
-      customer.email = email;
-      customer.gender = gender;
-      customer.healthPlanId = healthPlanId;
-      customer.number = number;
-      customer.state = state;
-      customer.city = city;
-      customer.zip = zip;
-      customer.cell = cell;
-      customer.home = home;
-      customer.work = work;
-      customer.facebook = facebook;
-      customer.instagram = instagram;
-      customer.twitter = twitter;
-      customer.linkedin = linkedin;
-      customer.sponsor = sponsor;
-    } else {
-      errorMessage = 'CPF inválido';
-    }
     onSubmit(id, customer, errorMessage);
   };
 
-  render() {
-    const {
-      addressType,
-      address,
-      address2,
-      neighborhood,
-      birth,
-      city,
-      rg,
-      cpf,
-      email,
-      gender,
-      healthPlanId,
-      name,
-      number,
-      state,
-      zip,
-      cell,
-      home,
-      work,
-      facebook,
-      instagram,
-      twitter,
-      linkedin,
-      sponsor,
-    } = this.state;
-    const { errorMessage, healthPlans, onClose, title } = this.props;
+  return (
+    <Modal size="large" open onClose={onClose}>
+      <Modal.Header content={title} />
+      <Modal.Content>
+        <Form onSubmit={handleSubmit} error={!!errorMessage}>
+          <Form.Group>
+            <Form.Input width={12} required autoFocus label="Nome" name="name" value={name} onChange={handleChange} />
+            <Form.Select
+              width={4}
+              label="Sexo"
+              name="gender"
+              value={gender}
+              options={genders}
+              placeholder="Sexo"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input label={'Nascimento'} width={3} type="date" name="birth" value={birth} onChange={handleChange} />
+            <Form.Input
+              width={3}
+              label={'CPF (apenas números)'}
+              name="cpf"
+              value={cpf}
+              pattern="\d*"
+              maxLength={11}
+              onChange={handleChange}
+            />
+            <Form.Input width={4} label={'Identidade'} name="rg" value={rg} onChange={handleChange} />
+            <Form.Input width={6} label={'Email'} name="email" value={email} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group>
+            <HealthPlanSelect width={7} onChange={handleChange} options={healthPlans} value={healthPlanId} />
+            <Form.Input width={3} label="Telefone Celular" name="cell" value={cell} onChange={handleChange} />
+            <Form.Input width={3} label="Telefone Residencial" name="home" value={home} onChange={handleChange} />
+            <Form.Input width={3} label="Telefone Comercial" name="work" value={work} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input width={4} label="Facebook" name="facebook" value={facebook} onChange={handleChange} />
+            <Form.Input width={4} label="Instagram" name="instagram" value={instagram} onChange={handleChange} />
+            <Form.Input width={4} label="Twitter" name="twitter" value={twitter} onChange={handleChange} />
+            <Form.Input width={4} label="LinkedIn" name="linkedin" value={linkedin} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Select
+              width={4}
+              label="Tipo"
+              name="addressType"
+              value={addressType}
+              options={addressTypes}
+              placeholder="Tipo"
+              onChange={handleChange}
+            />
+            <Form.Input width={10} label="Endereço" name="address" value={address} onChange={handleChange} />
+            <Form.Input width={2} label="Número" name="number" value={number} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input width={10} label="Complemento" name="address2" value={address2} onChange={handleChange} />
+            <Form.Input width={6} label="Bairro" name="neighborhood" value={neighborhood} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input width={8} label="Cidade" name="city" value={city} onChange={handleChange} />
+            <StateSelect value={state} onChange={handleChange} />
+            <Form.Input width={4} label="CEP" name="zip" value={zip} onChange={handleChange} />
+          </Form.Group>
+          <Sponsor cpf={sponsor} onChange={handleChange} />
+          <Message error header="Dados inválidos" content={errorMessage} />
+          <Button color="blue">
+            <Icon name="checkmark" /> Salvar
+          </Button>
+          <Button type="button" onClick={onClose}>
+            <Icon name="close" /> Cancelar
+          </Button>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  );
+};
 
-    return (
-      <Modal size="large" open onClose={onClose}>
-        <Modal.Header content={title} />
-        <Modal.Content>
-          <Form onSubmit={this.handleSubmit} error={!!errorMessage}>
-            <Form.Group>
-              <Form.Input
-                width={12}
-                required
-                autoFocus
-                label="Nome"
-                name="name"
-                value={name}
-                onChange={this.handleChange}
-              />
-              <Form.Select
-                width={4}
-                label="Sexo"
-                name="gender"
-                value={gender}
-                options={genders}
-                placeholder="Sexo"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Input
-                label={'Nascimento'}
-                width={3}
-                type="date"
-                name="birth"
-                value={birth}
-                onChange={this.handleChange}
-              />
-              <Form.Input
-                width={3}
-                label={'CPF (apenas números)'}
-                name="cpf"
-                value={cpf}
-                pattern="\d*"
-                maxLength={11}
-                onChange={this.handleChange}
-              />
-              <Form.Input width={4} label={'Identidade'} name="rg" value={rg} onChange={this.handleChange} />
-              <Form.Input width={6} label={'Email'} name="email" value={email} onChange={this.handleChange} />
-            </Form.Group>
-            <Form.Group>
-              <HealthPlanSelect width={7} onChange={this.handleChange} options={healthPlans} value={healthPlanId} />
-              <Form.Input width={3} label="Telefone Celular" name="cell" value={cell} onChange={this.handleChange} />
-              <Form.Input
-                width={3}
-                label="Telefone Residencial"
-                name="home"
-                value={home}
-                onChange={this.handleChange}
-              />
-              <Form.Input width={3} label="Telefone Comercial" name="work" value={work} onChange={this.handleChange} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Input width={4} label="Facebook" name="facebook" value={facebook} onChange={this.handleChange} />
-              <Form.Input width={4} label="Instagram" name="instagram" value={instagram} onChange={this.handleChange} />
-              <Form.Input width={4} label="Twitter" name="twitter" value={twitter} onChange={this.handleChange} />
-              <Form.Input width={4} label="LinkedIn" name="linkedin" value={linkedin} onChange={this.handleChange} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Select
-                width={4}
-                label="Tipo"
-                name="addressType"
-                value={addressType}
-                options={addressTypes}
-                placeholder="Tipo"
-                onChange={this.handleChange}
-              />
-              <Form.Input width={10} label="Endereço" name="address" value={address} onChange={this.handleChange} />
-              <Form.Input width={2} label="Número" name="number" value={number} onChange={this.handleChange} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Input
-                width={10}
-                label="Complemento"
-                name="address2"
-                value={address2}
-                onChange={this.handleChange}
-              />
-              <Form.Input
-                width={6}
-                label="Bairro"
-                name="neighborhood"
-                value={neighborhood}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Input width={8} label="Cidade" name="city" value={city} onChange={this.handleChange} />
-              <StateSelect value={state} onChange={this.handleChange} />
-              <Form.Input width={4} label="CEP" name="zip" value={zip} onChange={this.handleChange} />
-            </Form.Group>
-            <Sponsor cpf={sponsor} onChange={this.handleChange} />
-            <Message error header="Dados inválidos" content={errorMessage} />
-            <Button color="blue">
-              <Icon name="checkmark" /> Salvar
-            </Button>
-            <Button type="button" onClick={onClose}>
-              <Icon name="close" /> Cancelar
-            </Button>
-          </Form>
-        </Modal.Content>
-      </Modal>
-    );
-  }
-}
+export default Customer;
