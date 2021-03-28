c5f89e3a70f2abd4bed99ca3fd1514ffc7f878f6
Milton Castro <milton.castro@inovant.com.br>
2021-03-28 00:38:41 -0300

    refactor: convert HealthPlanItem to function component

--- src/health-plan/Item.js
+++ src/health-plan/Item.js
@@ -1,56 +1,56 @@
-import React, { Component } from 'react';
-import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 import { format } from 'cnpj';
+import React, { useState } from 'react';
+import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 
-export default class Item extends Component {
-  state = { cnpj: this.props.cnpj || '', name: this.props.name || '' };
+const HealthPlan = ({
+  cnpj: cnpjProps = '',
+  errorMessage,
+  id,
+  isNew,
+  name: nameProps = '',
+  onClose,
+  onSubmit,
+  title,
+}) => {
+  const [healthPlan, setHealthPlan] = useState({ cnpj: cnpjProps, name: nameProps });
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+  const handleChange = (e, { name, value }) => setHealthPlan((healthPlan) => ({ ...healthPlan, [name]: value }));
 
-  handleSubmit = () => {
-    const { name, cnpj } = this.state;
-    const { id, onSubmit } = this.props;
+  const { name, cnpj } = healthPlan;
+
+  const handleSubmit = () => {
     onSubmit(id, cnpj, name);
   };
 
-  render() {
-    const { name, cnpj } = this.state;
-    const { errorMessage, isNew, onClose, title } = this.props;
-    const isNotNew = !isNew;
-    return (
-      <Modal size="mini" open onClose={onClose}>
-        <Modal.Header content={title} />
-        <Modal.Content>
-          <Form onSubmit={this.handleSubmit} error={!!errorMessage}>
-            <Form.Input
-              required={isNew}
-              label={`CNPJ${isNew ? ' (apenas números)' : ''}`}
-              name="cnpj"
-              value={isNew ? cnpj : format(cnpj)}
-              autoFocus={isNew}
-              disabled={isNotNew}
-              pattern="\d*"
-              maxLength={14}
-              onChange={this.handleChange}
-            />
-            <Form.Input
-              required
-              label="Nome"
-              name="name"
-              value={name}
-              autoFocus={isNotNew}
-              onChange={this.handleChange}
-            />
-            <Message error header="Dados inválidos" content={errorMessage} />
-            <Button color="blue">
-              <Icon name="checkmark" /> Salvar
-            </Button>
-            <Button type="button" onClick={onClose}>
-              <Icon name="close" /> Cancelar
-            </Button>
-          </Form>
-        </Modal.Content>
-      </Modal>
-    );
-  }
-}
+  const isNotNew = !isNew;
+  return (
+    <Modal size="mini" open onClose={onClose}>
+      <Modal.Header content={title} />
+      <Modal.Content>
+        <Form onSubmit={handleSubmit} error={!!errorMessage}>
+          <Form.Input
+            required={isNew}
+            label={`CNPJ${isNew ? ' (apenas números)' : ''}`}
+            name="cnpj"
+            value={isNew ? cnpj : format(cnpj)}
+            autoFocus={isNew}
+            disabled={isNotNew}
+            pattern="\d*"
+            maxLength={14}
+            onChange={handleChange}
+          />
+          <Form.Input required label="Nome" name="name" value={name} autoFocus={isNotNew} onChange={handleChange} />
+          <Message error header="Dados inválidos" content={errorMessage} />
+          <Button color="blue">
+            <Icon name="checkmark" /> Salvar
+          </Button>
+          <Button type="button" onClick={onClose}>
+            <Icon name="close" /> Cancelar
+          </Button>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default HealthPlan;
